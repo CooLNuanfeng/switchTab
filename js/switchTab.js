3481aa5169e56dc625a6cde494679fdcc026d537
nuanfeng <1634898698@qq.com>
2014-06-30 18:03:04 +0800

    step_1

--- js/switchTab.js
+++ js/switchTab.js
@@ -1,8 +1,7 @@
 // JavaScript Document
 
 // 根据数量计算宽度 拖拽改变位置
-$(function(){
-	
+(function($){
 	function Factory(options){
 		
 		var tab = new SwitchTab(options);
@@ -11,16 +10,17 @@ $(function(){
 	}
 	
 	$.switchTab = $.fn.switchTab = Factory;
-	
 })(jQuery)
 
 function SwitchTab(){
 	
-	this.$switchTab = null;
+	this.$switchTab = $(this);
 	
 	this.settings = {
+		id : '', 
 		width : 500,
 		height:'auto',
+		className:'',
 		count : 3,
 		drag : false,
 		title :{
@@ -42,10 +42,56 @@ SwitchTab.prototype = {
 	
 	constructor : SwitchTab,
 	
-	init : function(){
+	init : function(options){
+		
+		$.extend(this.settings,options);
+		
+		this.createTab();
+	},
+	
+	createTab : function(){
+		
+		var title = [];
+		var content = [];
+		var titleList ='';
+		var boxDiv =''
+		for(var i=0; i<this.settings.count;i++){
+			
+			for(var attr in this.settings.title){
+				title.push(this.settings.title[attr])
+			}
+			for(var attr in this.settings.content){
+				content.push(this.settings.content[attr])
+			}
+			titleList +='<li '+(i==0?'class="active"':'')+'><a href="javascript:;" class="fa fa-paper-plane"></a><span>'+ title[i] +'</span></li>'
+			boxDiv += '<div class="tab_box '+(i==0?'active':'') +'"></div>'
+		};
+		
+		var html = '<div class="tab_title"><ul>'+titleList+'</ul></div>'+boxDiv
+		
+		$('#'+this.settings.id).addClass('tab '+this.settings.className).html(html)
+		
 	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
